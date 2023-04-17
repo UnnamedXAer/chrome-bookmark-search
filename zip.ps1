ac92a40b5951bb2c624a22558c70f287fcf00baa
UnnamedXAer <UnnamedXAer@gmail.com>
2023-04-17 12:49:48 +0200

    add script to zip required files

---
+++ zip.ps1
@@ -0,0 +1,24 @@
+
+Remove-Item "./build" -Recurse -Force -ErrorAction Ignore > $null
+
+pnpm run tsc > $null
+mkdir "./build" > $null
+
+mkdir "./build/icons"  > $null
+Get-ChildItem "./icons" -Recurse | Where-Object { ($_.name -like "*.png") } | Copy-Item -Destination "./build/icons/"
+
+
+Copy-Item "./manifest.json" "./build"
+Copy-Item "./src" -Recurse "./build"
+
+Get-ChildItem "./build/src" -Recurse | Where-Object { ($_.name -like "*.ts") -or ($_.name -like "*.js.map") } | Remove-Item
+
+$cwd = $pwd
+cd "./build"
+zip -FSr "../chrome-ext.zip" "./*" -9
+cd $cwd
+
+Remove-Item "./build" -Recurse -Force -ErrorAction Ignore > $null
+
+Write-Output "finished"
+
